f0a40e2a4a8d629a879540f1f040d11c871bc957
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2026-03-24 13:40:42 +0530

    fix : fixing util tests

--- scripts/validator/tests/test_utils.py
+++ scripts/validator/tests/test_utils.py
@@ -1,15 +1,17 @@
 from utils.utils import Utils
 
+
 def test_format_issue_with_row_and_single_column():
     issue = {
         "type": "error",
         "file": "data.json",
         "row": 5,
         "column": "name",
-        "message": "is invalid"
+        "message": "is invalid",
     }
     result = Utils.format_issue(issue)
     assert result == "[ERROR] data.json: Row 5, Column 'name' is invalid"
+
 
 def test_format_issue_with_row_and_multiple_columns():
     issue = {
@@ -17,35 +19,55 @@ def test_format_issue_with_row_and_multiple_columns():
         "file": "data.csv",
         "row": 10,
         "column": ["age", "dob"],
-        "message": "have conflicting values"
+        "message": "have conflicting values",
     }
     result = Utils.format_issue(issue)
-    assert result == "[WARNING] data.csv: Row 10, Columns [age, dob] have conflicting values"
+    assert (
+        result
+        == "[WARNING] data.csv: Row 10, Columns [age, dob] have conflicting values"
+    )
+
 
 def test_format_issue_without_row_and_column():
     issue = {
         "type": "error",
         "file": "config.json",
-        "message": "File not found or unreadable"
+        "message": "File not found or unreadable",
     }
     result = Utils.format_issue(issue)
     assert result == "[ERROR] config.json:  File not found or unreadable"
+
+
+def test_format_issue_without_rosw_and_column():
+    issue = {
+        "type": "error",
+        "file": "file.csv",
+        "row": None,
+        "column": ["header1", "header2"],
+        "message": "No rows found",
+    }
+    result = Utils.format_issue(issue)
+    assert result == "[ERROR] file.csv: , Columns [header1, header2] No rows found"
+
 
 def test_format_issue_without_row_with_column():
     issue = {
         "type": "error",
         "file": "schema.json",
         "column": ["header1", "header2"],
-        "message": "Duplicate columns"
+        "message": "Duplicate columns",
     }
     result = Utils.format_issue(issue)
-    assert result == "[ERROR] schema.json: Columns [header1, header2] Duplicate columns"
+    assert (
+        result == "[ERROR] schema.json: , Columns [header1, header2] Duplicate columns"
+    )
+
 
 def test_fits_in_int32():
     # Boundary values
     assert Utils.fits_in_int32(-2_147_483_648) is True
     assert Utils.fits_in_int32(2_147_483_647) is True
-    
+
     # Internal values
     assert Utils.fits_in_int32(0) is True
     assert Utils.fits_in_int32(1000) is True
